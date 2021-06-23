6f533aa61be6f459572be34dbe82d767306db507
Neecra <82094024+Neecra@users.noreply.github.com>
2021-06-23 23:13:06 +0200

    Update creditcard.lua

--- creditcard.lua
+++ creditcard.lua
@@ -1,5 +1,43 @@
-while true do
-    if disk.isPresent() then
-        fs.copy('./data.lua', 'disk/data.lua')
+function startsWith(text, prefix)
+    return text:find(prefix, 1, true) == 1
+end
+
+function clear()
+    term.clear()
+    term.setCursorPos(1,1)
+    term.setCursorBlink(true)
+end
+
+function main()
+    clear()
+    print('Bitte lege eine leere Karte ein!')
+    while true do
+        os.queueEvent("randomEvent")
+        os.pullEvent()
+        if disk.isPresent then
+            local label = disk.getLabel('top')
+            if label == nil then
+                label = 'Test'
+            end
+            if startsWith(label, 'Visa ') then
+                print('Diese Karte ist beteits eine Visa Karte!')
+                os.sleep(5)
+                return
+            end
+
+            local id = tostring(math.random(1000, 9999))
+            if disk.isPresent('top') then
+                disk.setLabel('top', 'Visa Card ID:'..tostring(id))
+                fs.copy('./data.lua', 'disk/data.lua')
+                clear()
+                print('Fertig!')
+                os.sleep(5)
+                return
+            end
+        end
     end
 end
+
+
+main()
+os.reboot()
